cbba135a9bcec1f2229a14e55bf99726c7f0452c
CheeseGuy100 <cheeseguy900@gmail.com>
2026-03-18 17:36:18 +0600

    Create composeApp/src/androidMain/kotlin/com/mangostore/ui/MainActivity.kt

---
+++ composeApp/src/androidMain/kotlin/com/mangostore/ui/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.mangostore.ui
+
+import android.content.Context
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+lateinit var appContext: Context
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        appContext = this
+        setContent {
+            App()
+        }
+    }
+}
